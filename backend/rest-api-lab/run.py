b81efa74436d22bb9178e48e15407ce3ac1a8c84
himangie05 <himangibhatt05@gmail.com>
2026-02-26 21:27:11 +0530

    Update: Finalized Experiment 8 README for submission

--- backend/rest-api-lab/run.py
+++
@@ -1,6 +0,0 @@
-from app import app
-
-if __name__ == "__main__":
-    # Running the app with debug=True enables auto-reloading 
-    # and provides a debugger in the browser if errors occur.
-    app.run(debug=True, port=5000)
